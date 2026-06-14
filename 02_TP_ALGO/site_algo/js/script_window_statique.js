b1e6e3f676abfd290691c1cb06d3d3493774b25c
Illia <ilija.airdrop@gmail.com>
2026-06-14 19:15:23 +0200

    4

--- 02_TP_ALGO/site_algo/js/script_window_statique.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-    const savedScroll = sessionStronge.getItem('scrollPosition');
-    if (savedScroll !== null) {
-        windowscrollTo(0, parseInt(savedScroll));
-    }
-});
-window.addEventListener('beforeunload', ()=> {
-    sessionStorage.setItem('scrollPosition', window.scroll.scrollY);
-})
