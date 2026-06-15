457def90db7d4ff4fa6b08c645b5bfccf6e1e56b
IlliasUA <ilija.airdrop@gmail.com>
2026-06-15 17:00:36 +0200

    Fin de journee JS-ALGO

--- 02_TP_ALGO/site_algo/JS/script_3.js
+++ 02_TP_ALGO/site_algo/JS/script_3.js
@@ -25,3 +25,15 @@ function myCalculeProduit() {
     }
     return false 
 }
+
+function myRangeAlphabet() {
+    let i_nom1, i_nom2, i_nom3;
+    i_nom1 = document.getElementById("i_nom1").value;
+    i_nom2 = document.getElementById("i_nom2").value;
+    i_nom3 = document.getElementById("i_nom3").value;
+    if (i_nom1 <= i_nom2 && i_nom2 <= i_nom3) {
+        document.getElementById("resulta").innerHTML = `Votre trois nom sont dans l’ordre alphabétique`;
+    } else
+        document.getElementById("resulta").innerHTML = `Votre trois nom ne sont pas dans l’ordre alphabétique`;
+        return false
+}
