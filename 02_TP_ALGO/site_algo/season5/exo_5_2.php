5c9a3d7eeda630547dcb30b66b6e16d87abeaf97
Illia <ilija.airdrop@gmail.com>
2026-06-22 20:14:29 +0200

    Mis a jour des exo S4 and S5

---
+++ 02_TP_ALGO/site_algo/season5/exo_5_2.php
@@ -0,0 +1,14 @@
+<?php
+$resulta= "";
+if(isset($_POST["iNombre"])) {
+    $iNombre= $_POST["iNombre"];
+    if ($iNombre < 10) {
+        $resulta= "(php) Plus grand!";
+     } else if ($iNombre > 20) {
+        $resulta= "(php) Plus petit!";   
+    } else {
+        $resulta= "(php) Saisie OK";    
+    }
+}
+require "exo_5_2.html";
+?>
