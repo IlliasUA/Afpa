5305a7b1faf0c28f5c9b582aa9758f93d0be4f0a
Illia <ilija.airdrop@gmail.com>
2026-06-27 23:03:17 +0200

    5.9-5.10

--- 02_TP_ALGO/site_algo/JS/script_5.js
+++ 02_TP_ALGO/site_algo/JS/script_5.js
@@ -287,7 +287,75 @@ function myNomberMax59 () {
 
 /* 5.10 js*/
 function myArticles () {
-     let sommeDonnee = document.getElementById("sNombre");
-     let votreMonnaie = sommeDevoir - sommeDonnee;
-     document.getElementById("resultat").innerHTML=`Votre monnaie est ${votreMonnaie}`;
+     let sommeDonnee = Number(document.getElementById("sNombre").value);
+     let sommeDevoir = window.sommeDevoir;
+     let nBillet10 = 0;
+     let nBillet5 = 0;
+     let nBillet1 = 0;
+     let votreMonnaie = sommeDonnee - sommeDevoir;
+     let resVotreMonnaie = votreMonnaie;
+     let resultates = "";
+
+     if(votreMonnaie >= 10) {
+          while (votreMonnaie >= 10) {
+               nBillet10++;  
+               votreMonnaie -= 10;
+          }
+     }
+     if(votreMonnaie >= 5) {
+          nBillet5++;
+          votreMonnaie -= 5;
+     }
+     if(votreMonnaie >= 1) {
+          nBillet1 = votreMonnaie;
+     } 
+     resultates = `Votre monnaie est ${resVotreMonnaie}€: <br>`;
+     if(nBillet10 > 0) {
+          resultates += `- ${nBillet10} billet de 10€<br>`;     
+     }
+     if(nBillet5 > 0) {
+          resultates += `- ${nBillet5} billet de 5€<br>`;
+     }
+     if(nBillet1 > 0) {
+          resultates += `- ${nBillet1} billet de 1€`;
+     }
+     document.getElementById("resultat").innerHTML = resultates;
+}
+
+
+/* 5.10 jq*/
+function myArticles_jq () {
+     let sommeDonnee = Number($("#sNombre").val());
+     let sommeDevoir = window.sommeDevoir;
+     let nBillet10 = 0;
+     let nBillet5 = 0;
+     let nBillet1 = 0;
+     let votreMonnaie = sommeDonnee - sommeDevoir;
+     let resVotreMonnaie = votreMonnaie;
+     let resultates = "";
+
+     if(votreMonnaie >= 10) {
+          while (votreMonnaie >= 10) {
+               nBillet10++;  
+               votreMonnaie -= 10;
+          }
+     }
+     if(votreMonnaie >= 5) {
+          nBillet5++;
+          votreMonnaie -= 5;
+     }
+     if(votreMonnaie >= 1) {
+          nBillet1 = votreMonnaie;
+     } 
+     resultates = `Votre monnaie est ${resVotreMonnaie}€: <br>`;
+     if(nBillet10 > 0) {
+          resultates += `- ${nBillet10} billet de 10€<br>`;     
+     }
+     if(nBillet5 > 0) {
+          resultates += `- ${nBillet5} billet de 5€<br>`;
+     }
+     if(nBillet1 > 0) {
+          resultates += `- ${nBillet1} billet de 1€`;
+     }
+     $("#resultat").html(`(jq) ${resultates}`);
 }
--- 02_TP_ALGO/site_algo/season5/exo_5_10.php
+++ 02_TP_ALGO/site_algo/season5/exo_5_10.php
@@ -1,19 +1,40 @@
 <?php
     $resultat= "";
     
-    if(isset($_POST["iNombre1"])) {
-        $max= $_POST["iNombre1"];
-        $fNombre= $_POST["fNombre"];
-        $iCount= 1;
-        for ($i=1; $i<=$fNombre; $i++) {
-            $iNombre = $_POST["iNombre" . $i];
+    if(isset($_POST["sNombre"])) {
+        $sommeDonnee= $_POST["sNombre"];
+        $sommeDevoir= $_POST["sommeDevoir"];
+        $nBillet10 = 0;
+        $nBillet5 = 0;
+        $nBillet1 = 0;
+        $votreMonnaie = $sommeDonnee - $sommeDevoir;
+        $resVotreMonnaie = $votreMonnaie;
+        $resultates = "";
 
-            if($max < $iNombre) {
-                $max = $iNombre;
-                $iCount = $i;                
-                }
+        if($votreMonnaie >= 10) {
+            while ($votreMonnaie >= 10) {
+                $nBillet10++;  
+                $votreMonnaie -= 10;
+            }
         }
-            $resultat= "(php) " . $max . " est plus grand numéro avec le champ de saisie n°" . $iCount; 
+        if($votreMonnaie >= 5) {
+            $nBillet5++;
+            $votreMonnaie -= 5;
+        }
+        if($votreMonnaie >= 1) {
+            $nBillet1 = $votreMonnaie;
+        } 
+        $resultates = "Votre monnaie est " . $resVotreMonnaie . "€: <br>";
+        if($nBillet10 > 0) {
+            $resultates .= "- " . $nBillet10 . "billet de 10€<br>";     
+        }
+        if($nBillet5 > 0) {
+            $resultates .= "- " . $nBillet5 . "billet de 5€<br>";
+        }
+        if($nBillet1 > 0) {
+            $resultates .= "- " . $nBillet1 . "billet de 1€";
+        }
+            $resultat= "(php) " . $resultates; 
     }
     require "exo_5_10.html";
 ?>
